8b916632589e1008de439fc8c29ad6b8a2b43909
tarek <53713568+usuck4@users.noreply.github.com>
2022-09-28 20:17:43 +0300

    Create SpeakHello.js

---
+++ module4-solution/SpeakHello.js
@@ -0,0 +1,16 @@
+(function(){
+
+ var helloSpeaker ={};
+
+
+var speakWord = "Hello";
+
+
+helloSpeaker.speak= function (name) {
+  console.log(speakWord + " " + name);
+}
+window.helloSpeaker=helloSpeaker;
+
+
+
+})();
